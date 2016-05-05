3e5e515f2902106a00c7d31d9f59fb728482a080
Alvinluipo <lualvin1234@gmail.com>
2016-05-05 14:13:21 -0400

    Done.

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,14 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-
+require 'dotenv'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
+Dotenv::Railtie.load
+
+HOSTNAME = ENV['HOSTNAME']
 
 module ApisAndFaradayReading
   class Application < Rails::Application
